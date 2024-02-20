2c1499e61829d8ac591af0c58190fb4d16c902f2
kishanhitk <kishansharma1231@gmail.com>
2024-02-20 10:19:36 +0530

    feat: convert to TS

---
+++ App.tsx
@@ -0,0 +1,29 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text
+        style={{
+          fontSize: 34,
+          fontWeight: "bold",
+          marginBottom: 20,
+        }}
+      >
+        Gita GPT 🦚
+      </Text>
+      <Text>Find wisdom in the solace of Shree Krishna</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
