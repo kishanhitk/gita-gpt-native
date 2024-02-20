96c98ec3a9b495f3ef77f3523ebe4d9e08618c49
kishanhitk <kishansharma1231@gmail.com>
2024-02-20 10:47:34 +0530

    feat: add Quicksand font

--- App.tsx
+++ App.tsx
@@ -1,19 +1,47 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
+import {
+  useFonts,
+  Quicksand_300Light,
+  Quicksand_400Regular,
+  Quicksand_500Medium,
+  Quicksand_600SemiBold,
+  Quicksand_700Bold,
+} from "@expo-google-fonts/quicksand";
 
 export default function App() {
+  let [fontsLoaded, fontError] = useFonts({
+    Quicksand_300Light,
+    Quicksand_400Regular,
+    Quicksand_500Medium,
+    Quicksand_600SemiBold,
+    Quicksand_700Bold,
+  });
+
+  if (!fontsLoaded && !fontError) {
+    return null;
+  }
+
   return (
     <View style={styles.container}>
       <Text
         style={{
           fontSize: 34,
-          fontWeight: "bold",
           marginBottom: 20,
+          fontFamily: "Quicksand_600SemiBold",
         }}
       >
         Gita GPT 🦚
       </Text>
-      <Text>Find sloce in the wisdom of Shree Krishna</Text>
+      <Text
+        style={{
+          fontSize: 18,
+          marginBottom: 20,
+          fontFamily: "Quicksand_400Regular",
+        }}
+      >
+        Find sloce in the wisdom of Shree Krishna
+      </Text>
       <StatusBar style="auto" />
     </View>
   );
