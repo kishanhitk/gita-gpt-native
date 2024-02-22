724e8ba390e638a6ffc2b28bcc5afee62870582c
kishanhitk <kishansharma1231@gmail.com>
2024-02-22 21:10:50 +0530

    feat: improve drawer

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -10,13 +10,14 @@ import { useFonts } from "expo-font";
 import React from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { Drawer } from "expo-router/drawer";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet, View, Image } from "react-native";
 import {
   DrawerContentScrollView,
   DrawerItem,
   DrawerItemList,
 } from "@react-navigation/drawer";
 import auth from "@react-native-firebase/auth";
+import StyledText from "../components/StyledText";
 
 export default function Layout() {
   let [fontsLoaded, fontError] = useFonts({
@@ -34,12 +35,36 @@ export default function Layout() {
   return (
     <GestureHandlerRootView style={{ flex: 1 }}>
       <Drawer
+        screenOptions={{}}
         drawerContent={(props) => {
           return (
             <DrawerContentScrollView
               {...props}
               contentContainerStyle={{ flexGrow: 1 }}
             >
+              <View
+                style={{
+                  padding: 20,
+                  marginVertical: 20,
+                  borderBottomWidth: 0.4,
+                  borderBottomColor: "rgba(0,0,0,0.2)",
+                }}
+              >
+                <Image
+                  source={require("../../assets/icon.png")}
+                  style={{ width: 100, height: 100, alignSelf: "center" }}
+                />
+                <StyledText
+                  style={{
+                    fontWeight: "600",
+                    fontSize: 25,
+                    textAlign: "center",
+                  }}
+                >
+                  Gita GPT
+                </StyledText>
+              </View>
+
               <DrawerItemList {...props} />
               <View style={{ marginTop: "auto" }}>
                 <DrawerItem label="Logout" onPress={() => auth().signOut()} />
