4177dc334fc2dced5c4e63796d1f0c8a2f71eb21
kishanhitk <kishansharma1231@gmail.com>
2024-02-22 11:39:15 +0530

    chore: cleanup

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -24,5 +24,3 @@ export default function Layout() {
   }
   return <Slot />;
 }
-
-// export default Slot;
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -139,7 +139,7 @@ export default function App() {
 
   return (
     <ScrollView>
-      <View className="mx-5 h-screen flex-1 flex-col items-center justify-center bg-white ">
+      <View className="px-5 h-screen flex-1 flex-col items-center justify-center bg-white ">
         {/* Mute Button */}
         <TouchableOpacity
           onPress={async () => {
