948508bf1befee7f8ea9333da9f296ce75ee4b40
kishanhitk <kishansharma1231@gmail.com>
2024-02-25 10:20:14 +0530

    feat: use nativewind 2

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [
-      ["babel-preset-expo", { jsxImportSource: "nativewind" }],
-      "nativewind/babel",
-    ],
-    plugins: [
-      // Required for expo-router
-      // "expo-router/babel",
-      "react-native-reanimated/plugin",
-    ],
+    presets: [["babel-preset-expo"]],
+    plugins: ["react-native-reanimated/plugin", "nativewind/babel"],
   };
 };
